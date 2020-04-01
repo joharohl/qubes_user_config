f46f9db5429eb6a17a2be528462c8e92079cce1f
Johan Rohlén <johan@mullvad.net>
2020-04-01 12:02:22 +0200

    Refactoring

--- install.sh
+++ install.sh
@@ -3,13 +3,17 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Installing bin folder into $HOME/bin"
-for f in $(ls $SCRIPT_DIR/bin); do
-  echo "$SCRIPT_DIR/bin/$f -> $HOME/bin/$f"
-  ln -fs $SCRIPT_DIR/bin/$f $HOME/bin/$f
+for f in "$SCRIPT_DIR"/bin/*; do
+  filename=$(basename "$f")
+  echo "$f -> $HOME/bin/$filename"
+  ln -fs "$f" "$HOME/bin/$filename"
 done
 
 echo "Installing dotfiles"
-for f in $(ls $SCRIPT_DIR/dotfiles); do
-  echo "$SCRIPT_DIR/dotfiles/$f -> $HOME/.$f"
-  ln -sf $SCRIPT_DIR/dotfiles/$f $HOME/.$f
+config_src=( gitconfig zshrc settings.json )
+config_dest=( .gitconfig .zshrc .config/Code/User/settings.json )
+n=$((${#config_src[@]} - 1))
+for i in $(seq 0 $n); do
+  echo "$SCRIPT_DIR/dotfiles/${config_src[$i]} -> $HOME/${config_dest[$i]}"
+  ln -sf "$SCRIPT_DIR/dotfiles/${config_src[$i]}" "$HOME/${config_dest[$i]}"
 done
